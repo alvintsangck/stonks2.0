51325a6a855794a2933a07abffab1179158a8aae
Alvin Tsang <alvintckw@gmail.com>
2022-02-23 17:59:26 +0800

    test react build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ConnectedRouter } from "connected-react-router";
 import "./App.css";
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import { history } from "./redux/store/history";
 
 function App() {
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 // import { useDispatch, useSelector } from "react-redux";
 // import { RootState } from "../redux/store/state";
 
-export default function NavBar() {
+export default function Navbar() {
 	// const dispatch = useDispatch();
 	// const user = useSelector((state: RootState) => state.auth);
 
