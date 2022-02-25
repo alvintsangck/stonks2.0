e028cc54263d11b1c209787b3739d3f044946928
David Yung <tree.yung.820@gmail.com>
2022-02-25 18:10:06 +0800

    add portfolio redux, service, table

--- frontend/src/redux/store/state.ts
+++ frontend/src/redux/store/state.ts
@@ -1,9 +1,11 @@
 import { RouterState } from "connected-react-router";
 import { AuthState } from "../auth/state";
 import { ThemeState } from "../theme/state";
+import { PortfolioState } from "../portfolio/state";
 
 export type RootState = {
 	auth: AuthState;
 	theme: ThemeState;
 	router: RouterState;
+	portfolio: PortfolioState;
 };
