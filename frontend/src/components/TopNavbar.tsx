80a2b1589a0fad23ef9c2d3dfb26efe3e1611abe
Alvin Tsang <alvintckw@gmail.com>
2022-03-12 20:24:34 +0800

    fix css

--- frontend/src/components/TopNavbar.tsx
+++ frontend/src/components/TopNavbar.tsx
@@ -5,40 +5,44 @@ import "../css/NavBar.css";
 import { Col, Container, Dropdown, Nav, Navbar, Row } from "react-bootstrap";
 import { SearchForm } from "./SearchForm";
 import { ColorTheme, TickerTape } from "react-ts-tradingview-widgets";
+import { env } from "../env";
 
 export default function NavBar() {
-	// const dispatch = useDispatch();
-	// const user = useSelector((state: RootState) => state.auth.user);
+	const user = useSelector((state: RootState) => state.auth.user);
 	const theme = useSelector((state: RootState) => state.theme.theme);
 
 	return (
 		<nav className="nav-bar">
 			<TickerTape colorTheme={theme as ColorTheme} copyrightStyles={{ parent: { display: "none" } }} />
 			<Container fluid>
-				<Row>
+				<Row className='align-items-center'>
 					<Col md={3}>
-						<div className="login">
-							<NavLink className="non-user" to="/login">
-								Login
-							</NavLink>
-							<NavLink className="non-user" to="/login">
-								Register
-							</NavLink>
-						</div>
-						<Dropdown className="user-only">
-							<Dropdown.Toggle className="user-avatar-btn">
-								<img className="user-avatar" src={"STONK.png"} alt="user-avatar" />
-							</Dropdown.Toggle>
-							<Dropdown.Menu>
-								<Dropdown.Item>
-									<NavLink className="dropdown-item" to="/setting">
-										User Setting
-									</NavLink>
-								</Dropdown.Item>
-								<Dropdown.Item>Change Theme</Dropdown.Item>
-								<Dropdown.Item>Log Out</Dropdown.Item>
-							</Dropdown.Menu>
-						</Dropdown>
+						{!user && (
+							<div className="login">
+								<NavLink className="non-user" to="/login">
+									Login
+								</NavLink>
+								<NavLink className="non-user" to="/login">
+									Register
+								</NavLink>
+							</div>
+						)}
+						{user && (
+							<Dropdown>
+								<Dropdown.Toggle className="user-avatar-btn">
+									<img className="user-avatar" src={`${env}/${user.payload.avatar}`} alt="user-avatar" />
+								</Dropdown.Toggle>
+								<Dropdown.Menu>
+									<Dropdown.Item>
+										{/* <NavLink className="dropdown-item" to="/setting"> */}
+											User Setting
+										{/* </NavLink> */}
+									</Dropdown.Item>
+									<Dropdown.Item>Change Theme</Dropdown.Item>
+									<Dropdown.Item>Log Out</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						)}
 					</Col>
 					<Col md={6} className="brand-name">
 						<NavLink to="/">
