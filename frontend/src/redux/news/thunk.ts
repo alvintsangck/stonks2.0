43de96451dd2af5d30d0f4cd53af65e23adc1887
Alvin Tsang <alvintckw@gmail.com>
2022-03-13 01:09:18 +0800

    add try catch

--- frontend/src/redux/news/thunk.ts
+++ frontend/src/redux/news/thunk.ts
@@ -4,15 +4,22 @@ import { getNewsAction, loadingNewsAction } from "./action";
 export function getNewsThunk() {
 	return async (dispatch: RootDispatch) => {
 		dispatch(loadingNewsAction());
-		const res = await fetch("https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=7", {
-			method: "GET",
-			headers: {
-				"x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
-				"x-rapidapi-key": "682db5aceamshb889cb938933b9bp159351jsna47daa8ade2e",
-			},
-		});
-		const result = await res.json();
-		const news = result.data;
-		dispatch(getNewsAction(news));
+		try {
+			const res = await fetch(
+				"https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=7",
+				{
+					method: "GET",
+					headers: {
+						"x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
+						"x-rapidapi-key": "682db5aceamshb889cb938933b9bp159351jsna47daa8ade2e",
+					},
+				}
+			);
+			const result = await res.json();
+			const news = result.data;
+			dispatch(getNewsAction(news));
+		} catch (error) {
+			console.log(error);
+		}
 	};
 }
