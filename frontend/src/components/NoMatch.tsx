bb040327ed5f65edb5040f9b0e479e2e04e01d75
Alvin Tsang <alvintckw@gmail.com>
2022-03-13 17:11:24 +0800

    add 404 img

--- frontend/src/components/NoMatch.tsx
+++ frontend/src/components/NoMatch.tsx
@@ -1,3 +1,23 @@
+import { push } from "connected-react-router";
+import { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { env } from "../env";
+
 export default function NoMatch() {
-	return <>404</>;
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		setTimeout(() => {
+			dispatch(push("/"));
+		}, 3000);
+	}, [dispatch]);
+
+	return (
+		<Container>
+			<h3>This page is not stonks.</h3>
+			<h4>Redirecting in 2 seconds...</h4>
+			<img src={`${env}/NOT_STONK.png`} alt="not stonk"></img>
+		</Container>
+	);
 }
