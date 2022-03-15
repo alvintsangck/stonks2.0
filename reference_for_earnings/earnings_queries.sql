3270eabee5baeb922a4a9dbfcfb10065fe5c7899
Ting Fung <fungting0921@gmail.com>
2022-03-16 01:51:21 +0800

    typos fixed

--- reference_for_earnings/earnings_queries.sql
+++ reference_for_earnings/earnings_queries.sql
@@ -8,7 +8,7 @@ join dim_year_quarters dyq on dyq.id = sse.year_quarter_id
 where sse.created_at in (select created_at from stock_earnings where created_at >= (now() - interval '1 DAY') and created_at <= now())
 order by date_id;
 
--- Earnings Releases in the last 10 days:
+-- Earnings Releases in the past 10 days:
 select sse.created_at, s.ticker, s."name", dyq.year, dyq.quarter, sse.eps_estimated, sse.eps_reported, sse.revenue_estimated, sse.revenue_reported  
 from stock_earnings sse 
 join stocks s on s.id = sse.stock_id 
