0121953533f6106435a64adac6563f60e9ee48cb
Alvin Tsang <alvintckw@gmail.com>
2022-03-17 04:24:53 +0800

    fix height

--- frontend/src/components/PowerBI.tsx
+++ frontend/src/components/PowerBI.tsx
@@ -7,8 +7,8 @@ export default function PowerBI() {
 			<Container fluid>
 				<iframe
 					title="sentiment indicators"
-					width="1140"
-					height="541.25"
+					width="100%"
+					height="800px"
 					src="https://app.powerbi.com/reportEmbed?reportId=d280ee63-32f4-428e-8e7c-ef01c70aa9f2&autoAuth=true&ctid=1cfbbd60-25f0-455a-976f-dfc4d172b5b9&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWVhc3QtYXNpYS1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
 					frameBorder="0"
 					allowFullScreen={true}
