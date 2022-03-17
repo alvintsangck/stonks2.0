20e17c16697aebbe4e9529747698b7efb63d3476
Ting Fung <fungting0921@gmail.com>
2022-03-17 13:19:46 +0800

    updated sql earnings release in the past 10 days

--- data/reference_for_earnings/earnings_queries.sql
+++ data/reference_for_earnings/earnings_queries.sql
@@ -18,8 +18,8 @@ select sse.created_at, s.ticker, s."name", dyq.year, dyq.quarter, sse.release_ti
 from stock_earnings sse 
 join stocks s on s.id = sse.stock_id 
 join dim_year_quarters dyq on dyq.id = sse.year_quarter_id
-where sse.created_at in (select created_at from stock_earnings where created_at >= (now() - interval '10 DAY') and created_at < now())
-order by date_id;
+where sse.created_at in (select created_at from stock_earnings where created_at >= (now() - interval '10 DAY') and created_at < (now() - interval '1 DAY'))
+order by date_id desc;
 
 -- Upcoming Earning Releases in the next 10 days:
 select sse.created_at, s.ticker, s."name", dyq.year, dyq.quarter, sse.release_time, sse.eps_estimated, sse.eps_reported, sse.revenue_estimated, sse.revenue_reported  
