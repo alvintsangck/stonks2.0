65fb8f0ae0b43acc3d65628396a990e94e042a26
Alvin Tsang <alvintckw@gmail.com>
2022-03-22 12:53:59 +0800

    change config

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -31,7 +31,7 @@ const knexConfigs = {
 			host: process.env.AWS_HOST,
 			database: process.env.POSTGRES_DB,
 			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
+			password: process.env.AWS_PASSWORD,
 		},
 		pool: {
 			min: 2,
