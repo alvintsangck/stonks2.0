a648f7f063d19a979c1d4962854d60f4b04678d2
Alvin Tsang <alvintckw@gmail.com>
2022-05-24 00:53:10 +0800

    change api key

--- frontend/src/redux/news/api.ts
+++ frontend/src/redux/news/api.ts
@@ -2,19 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { News } from "./state";
 
 export const newsApi = createApi({
-	reducerPath: "newsApi",
-	baseQuery: fetchBaseQuery({
-		baseUrl: "",
-		headers: {
-			"x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
-			"x-rapidapi-key": "eb76c9b688msha0363b0bcc4e8d8p1f0147jsn4c17370cbe91",
-		},
-	}),
-	endpoints: (builder) => ({
-		getNews: builder.query<News[], void>({
-			query: () => "https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=7",
-		}),
-	}),
+  reducerPath: "newsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "",
+    headers: {
+      "x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
+      "x-rapidapi-key": "5991559c7mshffbcf207c4b5b5fp14af29jsnf833c0b7551e",
+    },
+  }),
+  endpoints: (builder) => ({
+    getNews: builder.query<News[], void>({
+      query: () => "https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=7",
+    }),
+  }),
 });
 
 export const { useGetNewsQuery } = newsApi;
