989d8d88d38ada94ef1bd4349f5b92bd794e8c66
Alvin Tsang <alvintckw@gmail.com>
2022-05-25 23:11:13 +0800

    fix news api header

--- frontend/src/redux/news/api.ts
+++ frontend/src/redux/news/api.ts
@@ -5,14 +5,16 @@ export const newsApi = createApi({
   reducerPath: "newsApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "",
-    headers: {
-      "x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
-      "x-rapidapi-key": "5991559c7mshffbcf207c4b5b5fp14af29jsnf833c0b7551e",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Host", "seeking-alpha.p.rapidapi.com");
+      headers.set("X-RapidAPI-Key", "75991559c7mshffbcf207c4b5b5fp14af29jsnf833c0b7551e");
+      return headers;
     },
   }),
   endpoints: (builder) => ({
     getNews: builder.query<News[], void>({
       query: () => "https://seeking-alpha.p.rapidapi.com/news/v2/list-trending?until=0&since=0&size=7",
+      transformResponse: (res: { data: News[] }) => res.data,
     }),
   }),
 });
